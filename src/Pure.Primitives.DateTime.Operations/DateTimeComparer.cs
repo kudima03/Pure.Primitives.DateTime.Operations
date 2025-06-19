0d1be2c0e2b39edfccbb0491ac189e910318925e
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-19 10:13:25 +0300

    Implemented internal DateTimeComparer.

---
+++ src/Pure.Primitives.DateTime.Operations/DateTimeComparer.cs
@@ -0,0 +1,59 @@
+﻿using Pure.Primitives.Abstractions.DateTime;
+
+namespace Pure.Primitives.DateTime.Operations;
+
+internal sealed record DateTimeComparer : IComparer<IDateTime>
+{
+    public int Compare(IDateTime? first, IDateTime? second)
+    {
+        int result = first!.Year.NumberValue.CompareTo(second!.Year.NumberValue);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        result = first.Month.NumberValue.CompareTo(second.Month.NumberValue);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        result = first.Day.NumberValue.CompareTo(second.Day.NumberValue);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        result = first.Hour.NumberValue.CompareTo(second.Hour.NumberValue);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        result = first.Minute.NumberValue.CompareTo(second.Minute.NumberValue);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        result = first.Second.NumberValue.CompareTo(second.Second.NumberValue);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        result = first.Millisecond.NumberValue.CompareTo(second.Millisecond.NumberValue);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        result = first.Microsecond.NumberValue.CompareTo(second.Microsecond.NumberValue);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        return first.Nanosecond.NumberValue.CompareTo(second.Nanosecond.NumberValue);
+    }
+}
