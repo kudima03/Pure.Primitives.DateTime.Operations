f20e00b131eb5a0c3e38292f32c1c3428c86f87e
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-19 10:48:17 +0300

    Implemented BeforeCondition.

---
+++ src/Pure.Primitives.DateTime.Operations/BeforeCondition.cs
@@ -0,0 +1,58 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.Primitives.Abstractions.DateTime;
+
+namespace Pure.Primitives.DateTime.Operations;
+
+public sealed record BeforeCondition : IBool
+{
+    private readonly IComparer<IDateTime> _comparer;
+
+    private readonly IEnumerable<IDateTime> _values;
+
+    public BeforeCondition(params IDateTime[] values) : this(values.AsReadOnly()) { }
+
+    public BeforeCondition(IEnumerable<IDateTime> values) : this(values, new DateTimeComparer()) { }
+
+    private BeforeCondition(IEnumerable<IDateTime> values, IComparer<IDateTime> comparer)
+    {
+        _values = values;
+        _comparer = comparer;
+    }
+
+    bool IBool.BoolValue
+    {
+        get
+        {
+            using IEnumerator<IDateTime> enumerator = _values.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+            {
+                throw new ArgumentException();
+            }
+
+            IDateTime previous = enumerator.Current;
+
+            while (enumerator.MoveNext())
+            {
+                if (_comparer.Compare(previous, enumerator.Current) <= 0)
+                {
+                    return false;
+                }
+
+                previous = enumerator.Current;
+            }
+
+            return true;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
